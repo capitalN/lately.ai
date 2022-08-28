496f3a2a11b7f163c1b5f7bab2fdd56517d9c86d
debasishbihari <debasisbihari287@gmail.com>
2022-08-28 14:10:51 +0530

    updated links with navbar

--- component/navbar&footer.js
+++ component/navbar&footer.js
@@ -1,39 +1,39 @@
 let navbar = ()=>{
     return `
     <div id="logo">
-    <img style="width: 120px;" src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd8f444e5106349b4570e0b_lately-nav-logo.svg" alt="">
+    <a href="index.html"><img style="width: 120px;" src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd8f444e5106349b4570e0b_lately-nav-logo.svg" alt=""></a>
 </div>
 <div id="left-nav">
     <input type="checkbox" id="coll">
-    <label id="hamberger_icon" for="coll">☰</label>
+    <label id="hamberger_icon" for="coll"><img style="width: 35px;" src="https://cdn1.iconfinder.com/data/icons/basic-ui-elements-color-round/3/01-512.png" alt=""></a></label>
     <ul class="mainlist">
         <li>
             <input type="checkbox" id="coll-head1">
             <label for="coll-head1"><a>Product ▾</a></label>
                 <ul class="sublist" id="prod">
-                    <li class="one">
+                    <li>
                         <a href="">PARTNERS & INTEGRATIONS</a></h4>
                         <p>Find social media platform integrations and content extensions to enhance your AI content creation experience.</p>
                         <a href="">HubSpot Marketing Hub</a></h4>
                         <a href="">Hootsuite</a></h4>
                         <a href="">UpContent</a></h4>
                     </li>
-                    <li class="one">
-                        <a href="">AI CONTENT WRITER</a></h4>
+                    <li>
+                        <a href="./product/ai-content-writer.html">AI CONTENT WRITER</a></h4>
                         <p>Automatically writes and pre-tests content for your social media programs</p>
-                        <a href="">SOCIAL ANALYTICS</a></h4>
+                        <a href="./product/social-analytics.html">SOCIAL ANALYTICS</a></h4>
                         <p>Informs your content strategy and make your content more engaging with content analytics</p>
                     </li>
-                    <li class="one">
-                        <a href="">SOCIAL MEDIA MARKETING</a></h4>
+                    <li>
+                        <a href="./product/social-media-marketing.html">SOCIAL MEDIA MARKETING</a></h4>
                         <p>Everything you need to scale your social media marketing program</p>
-                        <a href="">SOCIAL SELLING</a></h4>
+                        <a href="./product/social-selling.html">SOCIAL SELLING</a></h4>
                         <p>Extend your social content across executives, sales, employees, franchises, and more</p>
                     </li>
-                    <li class="one">
-                        <a href="">VIDEO AUTOGENERATOR</a></h4>
+                    <li>
+                        <a href="./product/video-autogenerator.html">VIDEO AUTOGENERATOR</a></h4>
                         <p>Turn your company videos into bite-sized social media video clips and social media posts</p>
-                        <a href="">PARENT ACCOUNTS</a></h4>
+                        <a href="./product/parent-accounts.html">PARENT ACCOUNTS</a></h4>
                         <p>Easily manage parent-child social media accounts</p>
                     </li>
                 </ul>
@@ -42,36 +42,35 @@ let navbar = ()=>{
             <input type="checkbox" id="coll-head2">
             <label for="coll-head2"><a>Resource ▾</a></label>
             <ul class="sublist">
-                <li><a href="">Customer Stories</a></li>
-                <li><a href="">Lately Office Hours Sign-Up</a></li>
-                <li><a href="">Lately LIVE Replays</a></li>
-                <li><a href="">How to Position ANYTHING</a></li>
-                <li><a href="">Ebooks</a></li>
-                <li><a href="">Blog</a></li>
-                <li><a href="">Help Center</a></li>
+                <li><a href="./customer_stories/custom_stories.html">Customer Stories</a></li>
+                <li><a href="./Office_hours/office hour signup.html">Lately Office Hours Sign-Up</a></li>
+                <li><a href="./Live Reply/live_replys.html">Lately LIVE Replays</a></li>
+                <li><a href="./how_to_position/position.html">How to Position ANYTHING</a></li>
+                <li><a href="./Ebooks/ebooks.html">Ebooks</a></li>
+                <li><a href="./blog/blog.html">Blog</a></li>
+                <li><a href="./Help/help.html">Help Center</a></li>
             </ul>
         </li>
         <li>
             <input type="checkbox" id="coll-head3">
             <label for="coll-head3"><a>Company ▾</a></label>
             <ul class="sublist">
-                <li><a href="">About</a></li>
-                <li><a href="">Leadership</a></li>
-                <li><a href="">Press & News</a></li>
-                <li><a href="">Lately Professional Services</a></li>
-                <li><a href="">Contact Us</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="leadership.html">Leadership</a></li>
+                <li><a href="Press&News.html">Press & News</a></li>
+                <li><a href="latelyProfessionalservice.html">Lately Professional Services</a></li>
+                <li><a href="contectus.html">Contact Us</a></li>
             </ul>
-    
         </li>
         <li>
-            <a href="">Pricing</a>
+            <label><a href="pricing.html">Pricing</a></label>
         </li>
     </ul>
 </div>
    
 <div id="right-nav">
-    <a href=""><button>GET STARTED</button></a>
-    <a href=""">LOG IN</a>
+    <a href="signup.html"><button>GET STARTED</button></a>
+    <a href="logingateway.html">LOG IN</a>
 </div>
     `
 }
@@ -82,13 +81,11 @@ let footerFun = () => {
     <ul class="fmain">
     <ul class="fsublist">
         <li>COMPANY</li>
-        <li><a href="">About</a></li>
-        <li><a href="">Leadership</a></li>
-        <li><a href="">Professional Services</a></li>
-        <li><a href="">Press & News</a></li>
-        <li><a href="">Contact Us</a></li>
-        <li><a href="">Press & News</a></li>
-        <li><a href="">Contact Us</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="leadership.html">Leadership</a></li>
+        <li><a href="latelyProfessionalservice.html">Professional Services</a></li>
+        <li><a href="Press&News.html">Press & News</a></li>
+        <li><a href="contectus.html">Contact Us</a></li>
     </ul>
     <ul class="fsublist">
         <li>PARTNERS & INTEGRATIONS</li>
@@ -100,31 +97,31 @@ let footerFun = () => {
 <ul class="fmain">
     <ul class="fsublist">
         <li>PRODUCT</li>
-        <li><a href="">Pricing</a></li>
+        <li><a href="pricing.html">Pricing</a></li>
         <li><a href="">Product Overview</a></li>
-        <li><a href="">AI Social Content Writer</a></li>
-        <li><a href="">Social Media Marketing</a></li>
-        <li><a href="">Social Selling</a></li>
-        <li><a href="">Social Media Analytics</a></li>
-        <li><a href="">Sample AI Autogenerator</a></li>
-        <li><a href="">Autogenerator Chrome Extension</a></li>
-        <li><a href="">Parent Accounts</a></li>
+        <li><a href="./product/ai-content-writer.html">AI Social Content Writer</a></li>
+        <li><a href="./product/social-media-marketing.html">Social Media Marketing</a></li>
+        <li><a href="./product/social-selling.html">Social Selling</a></li>
+        <li><a href="./product/social-analytics.html">Social Media Analytics</a></li>
+        <li><a href="./product/video-autogenerator.html">Sample AI Autogenerator</a></li>
+        <li><a href="https://chrome.google.com/webstore/detail/generate-to-lately/bcghcdjbnjhbpokejekampjgnhchcapl?hl=en" target="_blank">Autogenerator Chrome Extension</a></li>
+        <li><a href="./product/parent-accounts.html">Parent Accounts</a></li>
     </ul>
 </ul>
 <ul class="fmain">
     <ul class="fsublist">
         <li>RESOURCES</li>
-        <li><a href="">Customer Stories</a></li>
-        <li><a href="">Ebooks</a></li>
-        <li><a href="">Blog</a></li>
-        <li><a href="">Help Center</a></li>
+        <li><a href="./customer_stories/customer_stories.html">Customer Stories</a></li>
+        <li><a href="./Ebooks/ebooks.html">Ebooks</a></li>
+        <li><a href="./blog/blog.html">Blog</a></li>
+        <li><a href="./Help/help.html">Help Center</a></li>
     </ul>
     <ul class="fsublist">
         <li>LATELY OFFICE HOURS</li>
-        <li><a href="">Lately Office Hours Sign-Up</a></li>
-        <li><a href="">Lately LIVE Replays</a></li>
-        <li><a href="">How to Position ANYTHING</a></li>
-        <li><a href="">Lately 101</a></li>
+        <li><a href="./Office_hours/office hour signup.html">Lately Office Hours Sign-Up</a></li>
+        <li><a href="./Live Reply/live_replys.html">Lately LIVE Replays</a></li>
+        <li><a href="./how_to_position/position.html">How to Position ANYTHING</a></li>
+        <li><a href="https://www.youtube.com/c/LatelyAI/videos" target="_blank">Lately 101</a></li>
     </ul>
 </ul>
 <ul class="fmain">
@@ -152,7 +149,7 @@ let footerFun = () => {
 
 let bottomFun = () => {
     return `
-    <a href="#"><img src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd7e81a75428f60b5ff0d00_lately-footer-logo.svg" alt=""></a>
+    <a href="index.html"><img src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd7e81a75428f60b5ff0d00_lately-footer-logo.svg" alt=""></a>
     <div id="social">
         <a href="https://www.facebook.com/LatelyAI/"><img src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd7ea98ca57d5a01f8b06ac_lately-footer-facebook-icon.svg" alt=""></a>
         <a href="https://twitter.com/LatelyAI"><img src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd7ea98d033dccb4e0c5c8a_lately-footer-twitter-icon.svg" alt=""></a>
