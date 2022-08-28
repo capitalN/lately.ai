0268930674b8974976c637218f8df21b60b50ad1
debasishbihari <debasisbihari287@gmail.com>
2022-08-28 12:16:39 +0530

    updated

--- component/navbar&footer.js
+++ component/navbar&footer.js
@@ -1,81 +1,171 @@
 let navbar = ()=>{
     return `
-    <div id="nav-container12">
-    <div id="wrapper">
-        <nav>
-            <ul id="nav">
-                <li id="logo1">
-                    <img src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd8f444e5106349b4570e0b_lately-nav-logo.svg" alt="">
-                </li>
-                <li class="product">
-                    <a href=""> Product</a> 
-                    <ul id="prod">
-                        <ul class="one" id="first">
-                            <h4>PARTNERS & INTEGRATIONS</h4>
-                            <p>Find social media platform integrations and content extensions to enhance your AI content creation experience.</p>
-                            <h4>HubSpot Marketing Hub</h4>
-                            <h4>Hootsuite</h4>
-                            <h4>UpContent</h4>
-                        </ul>
-                        <ul class="one">
-                            <h4>AI CONTENT WRITER</h4>
-                            <p>Automatically writes and pre-tests content for your social media programs</p>
-                            <h4>SOCIAL ANALYTICS</h4>
-                            <p>Informs your content strategy and make your content more engaging with content analytics</p>
-                        </ul>
-                        <ul class="one">
-                            <h4>SOCIAL MEDIA MARKETING</h4>
-                            <p>Everything you need to scale your social media marketing program</p>
-                            <h4>SOCIAL SELLING</h4>
-                            <p>Extend your social content across executives, sales, employees, franchises, and more</p>
-                        </ul>
-                        <ul class="one" id="last">
-                            <h4>VIDEO AUTOGENERATOR</h4>
-                            <p>Turn your company videos into bite-sized social media video clips and social media posts</p>
-                            <h4>PARENT ACCOUNTS</h4>
-                            <p>Easily manage parent-child social media accounts</p>
-                        </ul>
-                    </ul>
-                </li>
-                <li class="res">
-                    <a href="">Resources</a>
-                    <ul id="resource">
-                        <ul>
-                            <p>Customer Stories</p>
-                            <p>Lately Office Hours Sign-Up</p>
-                            <p>Lately LIVE Replays</p>
-                            <p>How to Position ANYTHING</p>
-                            <p>Ebooks</p>
-                            <p>Blog</p>
-                            <p>Help Center</p>
-                        </ul>
-                    </ul>
-                </li>
-                <li class="company">
-                    <a href="">Company</a>
-                    <ul id="comp">
-                        <ul>
-                            <p>About</p>
-                            <p>Leadership</p>
-                            <p>Press & News</p>
-                            <p>Lately Professional Services</p>
-                            <p>Contact Us</p>
-                        </ul>
-                    </ul>
-                </li>
-                <li class="price">
-                    <a href=""> Pricing</a> 
-                </li>
-                <li id="nav-login">
-                    <a href="">GET STARTED</a>
-                    <h3>LOG IN</h3>
-                </li>
-            </ul>
-        </nav>
-    </div>
+    <div id="logo">
+    <img style="width: 120px;" src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd8f444e5106349b4570e0b_lately-nav-logo.svg" alt="">
 </div>
-
+<div id="left-nav">
+    <input type="checkbox" id="coll">
+    <label id="hamberger_icon" for="coll">☰</label>
+    <ul class="mainlist">
+        <li>
+            <input type="checkbox" id="coll-head1">
+            <label for="coll-head1"><a>Product ▾</a></label>
+                <ul class="sublist" id="prod">
+                    <li class="one">
+                        <a href="">PARTNERS & INTEGRATIONS</a></h4>
+                        <p>Find social media platform integrations and content extensions to enhance your AI content creation experience.</p>
+                        <a href="">HubSpot Marketing Hub</a></h4>
+                        <a href="">Hootsuite</a></h4>
+                        <a href="">UpContent</a></h4>
+                    </li>
+                    <li class="one">
+                        <a href="">AI CONTENT WRITER</a></h4>
+                        <p>Automatically writes and pre-tests content for your social media programs</p>
+                        <a href="">SOCIAL ANALYTICS</a></h4>
+                        <p>Informs your content strategy and make your content more engaging with content analytics</p>
+                    </li>
+                    <li class="one">
+                        <a href="">SOCIAL MEDIA MARKETING</a></h4>
+                        <p>Everything you need to scale your social media marketing program</p>
+                        <a href="">SOCIAL SELLING</a></h4>
+                        <p>Extend your social content across executives, sales, employees, franchises, and more</p>
+                    </li>
+                    <li class="one">
+                        <a href="">VIDEO AUTOGENERATOR</a></h4>
+                        <p>Turn your company videos into bite-sized social media video clips and social media posts</p>
+                        <a href="">PARENT ACCOUNTS</a></h4>
+                        <p>Easily manage parent-child social media accounts</p>
+                    </li>
+                </ul>
+        </li>
+        <li>
+            <input type="checkbox" id="coll-head2">
+            <label for="coll-head2"><a>Resource ▾</a></label>
+            <ul class="sublist">
+                <li><a href="">Customer Stories</a></li>
+                <li><a href="">Lately Office Hours Sign-Up</a></li>
+                <li><a href="">Lately LIVE Replays</a></li>
+                <li><a href="">How to Position ANYTHING</a></li>
+                <li><a href="">Ebooks</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">Help Center</a></li>
+            </ul>
+        </li>
+        <li>
+            <input type="checkbox" id="coll-head3">
+            <label for="coll-head3"><a>Company ▾</a></label>
+            <ul class="sublist">
+                <li><a href="">About</a></li>
+                <li><a href="">Leadership</a></li>
+                <li><a href="">Press & News</a></li>
+                <li><a href="">Lately Professional Services</a></li>
+                <li><a href="">Contact Us</a></li>
+            </ul>
+    
+        </li>
+        <li>
+            <a href="">Pricing</a>
+        </li>
+    </ul>
+</div>
+   
+<div id="right-nav">
+    <a href=""><button>GET STARTED</button></a>
+    <a href=""">LOG IN</a>
+</div>
     `
 }
 
-export {navbar}
+
+let footerFun = () => {
+    return `
+    <ul class="fmain">
+    <ul class="fsublist">
+        <li>COMPANY</li>
+        <li><a href="">About</a></li>
+        <li><a href="">Leadership</a></li>
+        <li><a href="">Professional Services</a></li>
+        <li><a href="">Press & News</a></li>
+        <li><a href="">Contact Us</a></li>
+        <li><a href="">Press & News</a></li>
+        <li><a href="">Contact Us</a></li>
+    </ul>
+    <ul class="fsublist">
+        <li>PARTNERS & INTEGRATIONS</li>
+        <li><a href="">HubSpot Marketing Hub</a></li>
+        <li><a href="">Hootsuite</a></li>
+        <li><a href="">UpContent</a></li>
+    </ul>
+</ul>
+<ul class="fmain">
+    <ul class="fsublist">
+        <li>PRODUCT</li>
+        <li><a href="">Pricing</a></li>
+        <li><a href="">Product Overview</a></li>
+        <li><a href="">AI Social Content Writer</a></li>
+        <li><a href="">Social Media Marketing</a></li>
+        <li><a href="">Social Selling</a></li>
+        <li><a href="">Social Media Analytics</a></li>
+        <li><a href="">Sample AI Autogenerator</a></li>
+        <li><a href="">Autogenerator Chrome Extension</a></li>
+        <li><a href="">Parent Accounts</a></li>
+    </ul>
+</ul>
+<ul class="fmain">
+    <ul class="fsublist">
+        <li>RESOURCES</li>
+        <li><a href="">Customer Stories</a></li>
+        <li><a href="">Ebooks</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Help Center</a></li>
+    </ul>
+    <ul class="fsublist">
+        <li>LATELY OFFICE HOURS</li>
+        <li><a href="">Lately Office Hours Sign-Up</a></li>
+        <li><a href="">Lately LIVE Replays</a></li>
+        <li><a href="">How to Position ANYTHING</a></li>
+        <li><a href="">Lately 101</a></li>
+    </ul>
+</ul>
+<ul class="fmain">
+    <ul class="fsublist">
+        <li>INDUSTRIES</li>
+        <li><i>Every Size</i></li>
+        <li><a href="">Global Enterprises</a></li>
+        <li><a href="">Franchises</a></li>
+        <li><a href="">Small Businesses</a></li>
+    </ul>
+    <ul class="fsublist">
+        <li><i>Every Industry</i></li>
+        <li><a href="">Technology</a></li>
+        <li><a href="">Professional Services</a></li>
+        <li><a href="">Financial Services</a></li>
+        <li><a href="">Media & Entertainment</a></li>
+        <li><a href="">News & Public Agencies</a></li>
+        <li><a href="">Customer Brands</a></li>
+        <li><a href="">Education</a></li>
+        <li><a href="">Healthcare & Pharma</a></li>
+        <li><a href="">Social Media Agencies</a></li>
+    </ul>
+</ul>`
+}
+
+let bottomFun = () => {
+    return `
+    <a href="#"><img src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd7e81a75428f60b5ff0d00_lately-footer-logo.svg" alt=""></a>
+    <div id="social">
+        <a href="https://www.facebook.com/LatelyAI/"><img src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd7ea98ca57d5a01f8b06ac_lately-footer-facebook-icon.svg" alt=""></a>
+        <a href="https://twitter.com/LatelyAI"><img src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd7ea98d033dccb4e0c5c8a_lately-footer-twitter-icon.svg" alt=""></a>
+        <a href="https://www.linkedin.com/company/latelyai/"><img src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd7ea98a46fb9298adb10cf_lately-footer-linkedin-icon.svg" alt=""></a>
+        <a href="https://www.instagram.com/LatelyAI/"><img src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd7ea981e42d6cdd21dc804_lately-footer-instagram-icon.svg" alt=""></a>
+        <a href="https://www.youtube.com/LatelyAI"><img src="https://global-uploads.webflow.com/5fd3c52ce5bc147a1f007ca4/5fd7ea98d97acfb972a65b4a_lately-footer-youtube-icon.svg" alt=""></a></div>
+    </div>`
+}
+
+let termsFun = () => {
+    return`
+    <h7>Terms ● Privacy</h7><br>
+    <h7>Copyright © 2022 Lately, Inc. Stone Ridge, NY, USA</h7> `
+}
+
+export {navbar,footerFun,bottomFun,termsFun}
